53b37d24b79722a2c086e59c051bc2a49731186f
Isaack Motanya <isaackmotanya6@gmail.com>
2020-06-19 00:08:46 +0300

    update : is_verified fields set to 1, email_verified_at to current timestamp, is_verified to 1 and created_at to current timestamp by default

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -15,11 +16,13 @@ class UsersSeeder extends Seeder
         DB::table('users')->insert([
            [
                'first_name'=>'Tribore','last_name'=>'Health',
-               'phone_number'=>'0792651712','email'=>'contact@triboreheath.com',
+               'email_verified_at'=>Carbon::now()->toDateTimeString(),
+               'phone_number'=>'0712345676','email'=>'contact@triboreheath.com',
                'password'=>'$2y$12$ad7Spme/bC8oQt6JQdbvm./84xtYsvUnYMvHhAF6/anlRyl.J7WQS',
                'next_of_kin_first_name'=>'Tribore','next_of_kin_last_name'=>'Health',
                'next_of_kin_email'=>'info@triborehealth.com',
-               'next_of_kin_phone'=>'0792651712','role_id'=>'1'
+               'next_of_kin_phone'=>'07123456409','is_verified'=>'1','role_id'=>'1',
+               'created_at'=>Carbon::now()->toDateTimeString()
            ]
         ]);
     }
