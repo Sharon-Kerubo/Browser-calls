c2e0df3f657299f0b7d45776d71f2501a5a60609
Isaack Motanya <isaackmotanya6@gmail.com>
2020-06-19 00:15:16 +0300

    update : typo issue

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -17,7 +17,7 @@ class UsersSeeder extends Seeder
            [
                'first_name'=>'Tribore','last_name'=>'Health',
                'email_verified_at'=>Carbon::now()->toDateTimeString(),
-               'phone_number'=>'0712345676','email'=>'contact@triboreheath.com',
+               'phone_number'=>'0712345676','email'=>'admin@triborehealth.com',
                'password'=>'$2y$12$ad7Spme/bC8oQt6JQdbvm./84xtYsvUnYMvHhAF6/anlRyl.J7WQS',
                'next_of_kin_first_name'=>'Tribore','next_of_kin_last_name'=>'Health',
                'next_of_kin_email'=>'info@triborehealth.com',
